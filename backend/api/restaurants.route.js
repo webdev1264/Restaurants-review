51f5aaa482291d30b33f7149c043a77f5d903296
webdev1264 <webdev1264@gmail.com>
2023-01-09 23:08:20 +0400

    Add CRUD

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-
+import RestaurantsCtrl from "./restaurants.controller.js";
+import ReviewsCtrl from "./reviews.controller.js";
 const router = express.Router();
 
-router.route("/").get((req, res) => {
-  res.send("Hello world");
-});
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
+
+router
+  .route("/review")
+  .post(ReviewsCtrl.apiPostReview)
+  .put(ReviewsCtrl.apiUpdateReview)
+  .delete(ReviewsCtrl.apiDeleteReview);
 
 export default router;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import app from "./server.js";
 import mongodb from "mongodb";
 import dotenv from "dotenv";
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
@@ -17,6 +19,8 @@ MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
     process.exit(1);
   })
   .then(async (client) => {
+    await RestaurantsDAO.injectDB(client);
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`listening on port ${port}`);
     });
